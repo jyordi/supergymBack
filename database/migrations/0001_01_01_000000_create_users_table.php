c5fa4e39e983536c00a2d2b9d6e9ea87d0ef6dfb
raamses <raamseschavez21@gmail.com>
2025-10-30 11:48:16 -0600

    fix users objetivo

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,26 +4,31 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddFieldsToUsersTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('numero_usuario')->unique()->after('id');
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('numero_usuario')->unique();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
             $table->integer('edad')->nullable();
-            $table->enum('sexo',['M','F','Otro'])->nullable();
+            $table->enum('sexo', ['M','F','Otro'])->nullable();
             $table->decimal('peso',5,2)->nullable();
             $table->decimal('altura',5,2)->nullable();
-            $table->enum('nivel_conocimiento',['Principiante','Intermedio','Avanzado'])->default('Principiante');
-            $table->enum('objetivo',['Perder peso','Ganar músculo','Tonificación'])->nullable();
-            $table->enum('tipo_usuario',['Registrado','Invitado','Admin'])->default('Registrado');
+            $table->enum('nivel_conocimiento', ['Principiante', 'Intermedio', 'Avanzado'])->default('Principiante');
+            $table->enum('objetivo', ['Perder peso', 'Ganar músculo', 'Tonificación'])->nullable();
+            $table->enum('tipo_usuario', ['Registrado', 'Invitado', 'Admin'])->default('Registrado');
+            $table->rememberToken();
+            $table->timestamps();
         });
     }
 
-    public function down()
+    public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(['numero_usuario','edad','sexo','peso','altura','nivel_conocimiento','objetivo','tipo_usuario']);
-        });
+        Schema::dropIfExists('users');
     }
-}
+};
